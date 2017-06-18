eb743a97fc36f4a38e768cb021e9e91281ced2e3
Adrien Trouillaud <adrienjt@users.noreply.github.com>
2017-06-18 08:40:14 -0700

    fix index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 export {default as boolean} from './reducers/boolean';
 export {default as counter} from './reducers/counter';
+export {default as list} from './reducers/list';
 export {default as map} from './reducers/map';
-export {default as queue} from './reducers/queue';
 export {default as set} from './reducers/set';
-export {default as toggle} from './reducers/toggle';
-export {default as toggleSet} from './reducers/toggleSet';
 export {default as value} from './reducers/value';
